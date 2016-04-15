2bc7521526d5aba5eba97a8feb9fde4bcb94b3bb
Deryugin <deryugindenis@gmail.com>
2016-04-15 12:47:35 +0300

    (02) machine_learning: Bayes refactor

--- 02/machine_learning/bayes/main.py
+++ 02/machine_learning/bayes/main.py
@@ -16,64 +16,64 @@ util.init(data)
 
 c_cnt    = util.c_cnt
 o_cnt    = util.o_cnt
-simple_l = util.simple_l
-ranged_l = util.ranged_l
+simple_attr = util.simple_attr
+ranged_attr = util.ranged_attr
 
 # Learning
 data = np.array(data)
 np.random.shuffle(data)
 data = data[:100]
 
-for i in data:
-    t = int(i[13])
+for record in data:
+    t = int(record[13]) # 0   => Healthy
+                        # 1-4 => Has heart disease
     if t > 4:
         print "Error"
         exit()
     if t > 0:
         t = 1
 
-    for j in simple_l + ranged_l:
-        if abs(float(i[j]) + 9.0) < 0.01:
+    for attr in simple_attr + ranged_attr:
+        if abs(float(record[attr]) + 9.0) < 0.01:
             continue
-        cat = util.categorize(float(i[j]), j)
-        util.attr_sum[t][j] = util.attr_sum[t][j] + 1
-        o_cnt[j][cat][t] = o_cnt[j][cat][t] + 1
+        val = util.get_val(record, attr)
+        util.attr_sum[t][attr] += 1
+        o_cnt[attr][val][t] += 1
 
-    c_cnt[t] = c_cnt[t] + 1
+    c_cnt[t] += 1
 
 total = 0
 correct = 0
 slack = 0
 data = util.read_data(sys.argv[2])
-for i in data:
+for record in data:
     p = [1. * c_cnt[0] / (c_cnt[0] + c_cnt[1]),
             1. * c_cnt[1] / (c_cnt[0] + c_cnt[1])]
-    t = int(i[13])
+    t = int(record[13])
     if t > 4:
         print "Error"
         exit()
     if t > 0:
         t = 1
 
-    for j in simple_l + ranged_l:
-        if abs(float(i[j]) + 9.0) < 0.01:
+    for attr in simple_attr + ranged_attr:
+        # If value equals -9 then the value is not
+        # specified
+        if abs(float(record[attr]) + 9.0) < 0.01:
             continue
 
-        val = util.categorize(float(i[j]), j)
+        val = util.get_val(record, attr)
 
         for k in [0, 1]:
-            sm = util.attr_sum[k][j];
+            sm = util.attr_sum[k][attr];
             if sm == 0:
                 p[k] = p[k] * 0.5
             else:
-                p[k] = p[k] * o_cnt[j][val][k] / sm
+                p[k] = p[k] * o_cnt[attr][val][k] / sm
 
-    if p[0] + p[1] < 0.0000000001:
-        slack = slack + 1
-    if (t == 0 and p[0] > p[1]) or (t > 0 and p[1] > p[0]):
+    if p[t] > p[t - 1]:
         correct = correct + 1
 
     total = total + 1
 
 print 1. * correct / total
-print "Slack: " + str(slack) + " out of " + str(total)
--- 02/machine_learning/bayes/util.py
+++ 02/machine_learning/bayes/util.py
@@ -2,53 +2,54 @@
 
 import sys
 
-mx = []
-mn = []
+max_val = []
+min_val = []
 required_fields = [2, 3, 8, 9, 11, 15, 18, 31, 37, 39, 40, 43, 50, 57]
 attr_n = len(required_fields)
 inf = 99999999.
 
-simple_l = [1, 2, 5, 6, 8, 10, 11, 12]
-ranged_l = [0, 3, 4, 7, 9]
+simple_attr = [1, 2, 5, 6, 8, 10, 11, 12] # No need to break into intervals
+ranged_attr = [0, 3, 4, 7, 9]             # Need to break into intervals
 
 c_cnt = [0, 0]
 o_cnt = []
 
 attr_sum = [[],[]]
 
-cat_max = 15
+attr_max = 15
 
-def categorize(x, cat):
-    if cat in simple_l:
+def get_val(record, attr):
+    x = float(record[attr])
+    if attr in simple_attr:
         return int(x)
 
-    if x < mn[cat] or abs(mx[cat] - mn[cat]) < 0.0001:
+    if x < min_val[attr] or abs(max_val[attr] - min_val[attr]) < 0.0001:
         return 0
 
-    if x > mx[cat]:
-        return cat_max - 1
+    if x > max_val[attr]:
+        return attr_max - 1
 
-    return int((x - mn[cat] - 1/inf) / (mx[cat] - mn[cat]) * cat_max)
+    return int((x - min_val[attr] - 1/inf) / (max_val[attr] - min_val[attr]) * attr_max)
 
 def init(data):
     for i in range(0, attr_n):
-        mx.append(-inf)
-        mn.append(inf)
+        max_val.append(-inf)
+        min_val.append(inf)
 
     for k in range(0, attr_n):
         o_cnt.append([])
-        for j in range(0, max(100, cat_max)):
+        for j in range(0, max(100, attr_max)):
             o_cnt[k].append([0, 0])
             attr_sum[0].append(0)
             attr_sum[1].append(0)
 
     for i in data:
-        for j in ranged_l:
-            val = float(i[j])
-            if mx[j] < val:
-                mx[j] = val
-            if mn[j] > val:
-                mn[j] = val
+        for attr in ranged_attr:
+            val = float(i[attr])
+            if max_val[attr] < val:
+                max_val[attr] = val
+            if min_val[attr] > val:
+                min_val[attr] = val
 
 def print_usage():
     print "USAGE: " + sys.argv[0] + " [TRAIN_FILE TEST_FILE]"
