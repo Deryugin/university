1b1b83ed158673c4b0e78cb5e7a7b9fe0a53c135
Deryugin <deryugindenis@gmail.com>
2016-04-15 11:53:44 +0300

    (02) machine_learning: Add help for SVM-light script

--- 02/machine_learning/svm/run.sh
+++ 02/machine_learning/svm/run.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-
-if (($# < 2))
+set -x
+if (($# < 1))
 then
         echo "USAGE: $0 [Directory of the set]"
         echo "For example: $0 ./Exp_8564_8567"
+        exit
+fi
+
+if [ ! -f "./svm_learn" ]
+then
+        echo "No ./svm_learn found in current directory"
+        echo "Extract svm_light for Linux in current directory"
+        exit
+fi
+
+if [ ! -f "./svm_classify" ]
+then
+        echo "No ./svm_learn found in current directory"
+        echo "Extract svm_light for Linux in current directory"
         exit
 fi
 
@@ -12,18 +26,18 @@ VECTOR_FILE="$FOLDER/vectors.dat"
 EXAMPLE_FILE=$VECTOR_FILE
 MODEL_FILE="$FOLDER/model"
 
-OPTIONS="-z c"
+cp $VECTOR_FILE inp_tmp
+VECTOR_FILE=inp_tmp
 
+OPTIONS="-z c -q 20 -w 0.001 -k 100 -t 1"
+
+# Remove comments
 sed -i '/#./ d' $VECTOR_FILE
 
 SAMPLE_SZ=`wc -l $VECTOR_FILE | sed 's/ .*//'`
 
-grep \"^-1\" $VECTOR_FILE > c0
-grep \"^1\" $VECTOR_FILE > c1
-
 # Divide data set into train and learn subsets
-TRAIN_COEF=0.75 # == TRAIN_NUM / (TRAIN_NUM + TEST_NUM)
-
+TRAIN_COEF=0.9
 TRAIN_NUM=`echo "$TRAIN_COEF * $SAMPLE_SZ / 1" | bc`
 TEST_NUM=$(($SAMPLE_SZ - $TRAIN_NUM))
 
@@ -32,9 +46,10 @@ head -n $TRAIN_NUM vec_tmp > ex_f
 tail -n $TEST_NUM vec_tmp > test_f
 
 ./svm_learn $OPTIONS ex_f $MODEL_FILE
-
 ./svm_classify test_f $MODEL_FILE
 
+# Remove tmp files
+rm inp_tmp
 rm vec_tmp
 rm test_f
 rm ex_f
